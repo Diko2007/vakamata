52bacf35fa0bd4d35f2a2dc876d775aa38ae7ff9
Diko2007 <rozmetov2003@mail.ru>
2026-01-19 15:30:26 +0500

    Add files via upload

---
+++ #algoritmi s vetvlenien.py
@@ -0,0 +1,9 @@
+#algoritmi s vetvlenien
+x = -10
+
+if x > 0:
+    print(f"{x} - plus")
+elif x < 0:
+    print(f"{x} - minus")
+else:
+    print(f"{x} - same")
---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+a = int("12")
+b = int("13")
+sum_result = a+b
+diff_result = a-b
+prod_result = a*b
+print("summa:",sum_result)
+print("raznost:",diff_result)
+print("proizvedenie:",prod_result)
