23ff9bf2d40088a378e9ac835a1a3a367c3da0a7
vakamata <rozmetov2003@mail.ru>
2026-01-19 15:52:12 +0500

    move folder

---
+++ lab 1/n = 10.py
@@ -0,0 +1,6 @@
+n = 10
+total = 0
+for i in range(10, + 1)
+  total +=i 
+ 
+ print("summa ot 1 do",n,"ravna",total)
--- n = 10.py
+++
@@ -1,6 +0,0 @@
-n = 10
-total = 0
-for i in range(10, + 1)
-  total +=i 
- 
- print("summa ot 1 do",n,"ravna",total)
